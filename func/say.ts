b09a485c1df22a6df9f3a41fc607568c3f386b90
SICout9010 <fivrewgsd@gmail.com>
2025-09-14 03:12:28 +0700

    Update say.ts

--- func/say.ts
+++ func/say.ts
@@ -1,32 +1,34 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder, TextChannel } from 'discord.js';
+import {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  TextChannel,
+} from "discord.js";
 
 export const data = new SlashCommandBuilder()
   .setName("say")
   .setDescription("พูดคำว่า")
-  .addChannelOption(option =>
-    option.setName('channel')
-        .setDescription('ช่องที่จะพูด')
-        .setRequired(true)
-    )
-  .addStringOption(option =>
-    option.setName('message')
-        .setDescription('คำว่า')
-        .setRequired(true)
-    );
+  .addChannelOption((option) =>
+    option.setName("channel").setDescription("ช่องที่จะพูด").setRequired(true)
+  )
+  .addStringOption((option) =>
+    option.setName("title").setDescription("หัวข้อ").setRequired(false)
+  )
+  .addStringOption((option) =>
+    option.setName("message").setDescription("คำว่า").setRequired(true)
+  );
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-    const channel = interaction.options.getChannel('channel', true)
-    const message = interaction.options.getString('message', true)
-    const user = interaction.user
-    const embed = new EmbedBuilder()
-        .setColor(0x0099ff) 
-        .setTitle("⭐️ Say by " + user.username)
-        .setDescription(
-            message,
-        )
-        .setFooter({ text: user.username })
-        .setThumbnail((interaction.client.user?.displayAvatarURL() || "") || ""); // <-- small image top-right
+  const channel = interaction.options.getChannel("channel", true);
+  const message = interaction.options.getString("message", true);
+  const title = interaction.options.getString("title") || "TobeIT'69";
+  const embed = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTitle(title)
+    .setDescription(message)
+    .setFooter({ text: "TobeIT'69" })
+    .setThumbnail(interaction.client.user?.displayAvatarURL() || "" || ""); // <-- small image top-right
 
-    await (channel as TextChannel).send({ embeds: [embed] });
-    await interaction.reply({ content: "พูดไปแล้วจ้า", ephemeral: true });
+  await (channel as TextChannel).send({ embeds: [embed] });
+  await interaction.reply({ content: "พูดไปแล้วจ้า", ephemeral: true });
 }
